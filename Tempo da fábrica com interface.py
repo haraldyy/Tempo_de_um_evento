c1b67057ba30788b0d4a5429f6058debc3cc4a34
haraldyy <69537970+haraldyy@users.noreply.github.com>
2020-08-21 17:37:45 -0300

    Add files via upload

---
+++ Tempo da fábrica com interface.py
@@ -0,0 +1,13 @@
+def quantidade(tempo):
+    segundo = int(tempo // 3600)
+    minuto =int((tempo % 3600)// 60)
+    hora =int((tempo % 3600) % 60)
+    return (f'{segundo}:{minuto}:{hora}')
+
+def main():
+    tempo = int(input('Digite o tempo do evento em segundos:'))
+    tempot =quantidade(tempo)
+    print(quantidade(tempo))
+
+if __name__=='__main__':
+    main()
